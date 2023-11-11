479f89241608778c3896cf313f034ac410ee56ae
dejourford <dejourford@my.unt.edu>
2023-11-11 17:33:51 -0600

    styled login page and added password type to input

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,8 +1,14 @@
+import "./loginpage.css"
+
 export default function LoginPage() {
 
     return (
-        <div>
-            <h2>LoginPage</h2>
-        </div>
+        <section className="login-section">
+            <h1>stock</h1>
+            <div className="input-section">
+                <input type="text" placeholder="username"/>
+                <input type="password" placeholder="password" />
+            </div>
+        </section>
     );
 };
