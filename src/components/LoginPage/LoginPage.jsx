41a5e3d5f6633931f4cac08c7f9b8541c64e6e01
dejourford <dejourford@my.unt.edu>
2023-11-11 18:03:24 -0600

    add h4 header and log in button

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -6,8 +6,10 @@ export default function LoginPage() {
         <section className="login-section">
             <h1>stock</h1>
             <div className="input-section">
+                <h4>Log In to Stock</h4>
                 <input type="text" placeholder="username"/>
                 <input type="password" placeholder="password" />
+                <button>Log In</button>
             </div>
         </section>
     );
