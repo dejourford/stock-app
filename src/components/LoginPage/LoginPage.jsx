beb852244bd2c42e3c48a882d2e24e71f60a91d6
dejourford <dejourford@my.unt.edu>
2023-11-11 18:13:17 -0600

    added titles to inputs

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -7,8 +7,16 @@ export default function LoginPage() {
             <h1>stock</h1>
             <div className="input-section">
                 <h4>Log In to Stock</h4>
-                <input type="text" placeholder="username"/>
-                <input type="password" placeholder="password" />
+                <div className="input-group">
+                    <p className="input-group-title">Username</p>
+                    <input type="text"/>
+                </div>
+                
+                <div className="input-group">
+                    <p className="input-group-title">Password</p>
+                    <input type="password"/>
+                </div>
+                
                 <button>Log In</button>
             </div>
         </section>
